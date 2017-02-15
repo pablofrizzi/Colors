739f3629113aa352f007261ea15b5e2370f1961d
pablofrizzi <pablofrizzi@gmail.com>
2017-02-15 11:13:02 +0100

    color added, id's deleted

--- js/main.js
+++ js/main.js
@@ -8,28 +8,25 @@ var menuToggle = function() {
 
 var colorsArr = [
   {
-    id: "colorZero",
     color: "white"
   },
   {
-    id: "colorOne",
     color: "#C56FE5"
   },
   {
-    id: "colorTwo",
     color: "#A9F0E2"
   },
   {
-    id: "colorThree",
     color: "#F06381"
   },
   {
-    id: "colorFour",
     color: "#5BA0E5"
   },
   {
-    id: "colorFive",
     color: "gold"
+  },
+  {
+    color: "#1abc9c"
   }
 ];
 
@@ -39,12 +36,10 @@ var colorChanger = function() {
   colorsArr.forEach(function(color){
     var element = $('<li>')
       .addClass('colorDot')
-      .attr('id', color.id)
       .css('background', color.color)
       .click(function(){
         mainColor.style.background = color.color;
       });
-
     colorList.append(element);
   });
 }
