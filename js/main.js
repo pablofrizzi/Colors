8499282052f3fed5da607d6049357017a93a1eb5
pablofrizzi <pablofrizzi@gmail.com>
2017-02-16 14:17:27 +0100

    objects -> strings in array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var colorsArr = ["#FFF", "#C56FE5", "#A9F0E2", "#F06381", "#5BA0E5", "#FFD700", "#1abc9c"];
+
 var menuToggle = function() {
   $('.button').click(function() {
     $('.colorPicker').toggle();
@@ -21,7 +23,7 @@ var appendColor = function() {
   colorsArr.forEach(function(color){
     var element = $('<li>')
       .addClass('colorDot')
-      .css('background', color.color);
+      .css('background', color);
 
     colorList.append(element);
   });
@@ -34,27 +36,3 @@ function main() {
 }
 
 $(document).ready(main);
-
-var colorsArr = [
-  {
-    color: "white"
-  },
-  {
-    color: "#C56FE5"
-  },
-  {
-    color: "#A9F0E2"
-  },
-  {
-    color: "#F06381"
-  },
-  {
-    color: "#5BA0E5"
-  },
-  {
-    color: "gold"
-  },
-  {
-    color: "#1abc9c"
-  }
-];
